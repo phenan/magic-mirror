7da78ff70767d8f89a4c902723821fab38e933d3
Kazuhiro Ichikawa <ichikawa@five-corp.com>
2020-02-02 17:07:07 +0900

    rewrite Generic instance declarations

--- src/main/scala/com/phenan/generic/Generic.scala
+++ src/main/scala/com/phenan/generic/Generic.scala
@@ -13,42 +13,38 @@ trait Generic [T, R] {
 object Generic {
   type ProductMirror [T, R <: Product] = Mirror.ProductOf[T] { type MirroredElemTypes = R }
 
-  class FromProductMirror [T <: Product, R <: Product] (val mirror: ProductMirror[T, R]) extends Generic[T, R] {
-    def from (r: R): T = {
-      mirror.fromProduct(r)
-    }
-    def to (t: T): R = {
-      Tuple.fromProductTyped(t)(given mirror)
-    }
-  }
-
   type SumMirror [T, R] = Mirror.SumOf[T] { type MirroredElemTypes = R }
+
+  type SingletonMirror [T] = Mirror.ProductOf[T] { type MirroredElemTypes = Unit }
 
   type ElementsOfCoproduct[C <: Coproduct] = C match {
     case CNil     => Product0
     case e :+: es => e *: ElementsOfCoproduct[es]
   }
+}
 
-  class FromSumMirror [T, R <: Coproduct] (val mirror: SumMirror[T, ElementsOfCoproduct[R]]) extends Generic[T, R] {
-    def from (r: R): T = {
-      r.getUnTypedValue.asInstanceOf[T]
-    }
-    def to (t: T): R = {
-      val ordinal = mirror.ordinal(t.asInstanceOf[mirror.MirroredMonoType])
-      Coproduct[R].applyUnsafe(ordinal, t)
-    }
+given genericFromSingletonMirror [T] (given mirror: Generic.SingletonMirror[T]): Generic[T, Unit] {
+  def from (r: Unit): T = {
+    mirror.fromProduct(EmptyProduct)
   }
+  def to (t: T): Unit = ()
+}
 
-  type SingletonMirror [T] = Mirror.ProductOf[T] { type MirroredElemTypes = Unit }
-
-  class FromSingletonMirror [T] (val mirror: SingletonMirror[T]) extends Generic[T, Unit] {
-    def from (r: Unit): T = {
-      mirror.fromProduct(EmptyProduct)
-    }
-    def to (t: T): Unit = ()
+given genericFromProductMirror [T <: Product, R <: Product] (given mirror: Generic.ProductMirror[T, R]): Generic[T, R] {
+  def from (r: R): T = {
+    mirror.fromProduct(r)
+  }
+  def to (t: T): R = {
+    Tuple.fromProductTyped(t)(given mirror)
   }
 }
 
-given [T] (given mirror: Generic.SingletonMirror[T]): Generic[T, Unit] = new Generic.FromSingletonMirror(mirror)
-given [T <: Product, R <: Product] (given mirror: Generic.ProductMirror[T, R]): Generic[T, R] = new Generic.FromProductMirror(mirror)
-given [T, R <: Coproduct] (given mirror: Generic.SumMirror[T, Generic.ElementsOfCoproduct[R]]): Generic[T, R] = new Generic.FromSumMirror(mirror)
+given genericFromSumMirror [T, R <: Coproduct] (given mirror: Generic.SumMirror[T, Generic.ElementsOfCoproduct[R]]): Generic[T, R] {
+  def from (r: R): T = {
+    r.getUnTypedValue.asInstanceOf[T]
+  }
+  def to (t: T): R = {
+    val ordinal = mirror.ordinal(t.asInstanceOf[mirror.MirroredMonoType])
+    Coproduct[R].applyUnsafe(ordinal, t)
+  }
+}
