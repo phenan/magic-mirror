0153e93023cc5c7c10274a50dfebe1deb9ceac85
Kazuhiro Ichikawa <ichikawa@five-corp.com>
2020-02-06 22:32:01 +0900

    improve implementation of MirrorLens

--- src/main/scala/com/phenan/lens/MirrorLens.scala
+++ src/main/scala/com/phenan/lens/MirrorLens.scala
@@ -8,18 +8,20 @@ import scala.language.dynamics
 object MirrorLens {
   def apply [T <: Product] (given mirror: Mirror.ProductOf[T]): MirrorLensBuilder[T, mirror.MirroredElemTypes, mirror.MirroredElemLabels] = new MirrorLensBuilder(mirror)
 
-  class MirrorLensBuilder [T <: Product, ElemTypes <: Tuple, ElemLabels <: Tuple] (val mirror: Mirror.ProductOf[T]) extends Dynamic {
+  class MirrorLensBuilder [T <: Product, ElemTypes <: Tuple, ElemLabels <: Tuple] (mirror: Mirror.ProductOf[T]) extends Dynamic {
     def selectDynamic [L <: Singleton] (label: L)(given proof: L <:< Tuples.UnionOf[ElemLabels], tupleLens: TupleLens[ElemTypes, Tuples.IndexOf[ElemLabels, L]]): MirrorLens[T, ElemTypes, Tuples.IndexOf[ElemLabels, L]] = {
       new MirrorLens(mirror, tupleLens)
     }
   }
 }
 
-class MirrorLens [T <: Product, ElemTypes <: Tuple, I <: Int] (val mirror: Mirror.ProductOf[T], tupleLens: TupleLens[ElemTypes, I]) extends Lens[T, Tuple.Elem[ElemTypes, I]] {
-  def get (t: T): Tuple.Elem[ElemTypes, I] = {
+class MirrorLens [T <: Product, ElemTypes <: Tuple, I <: Int] (mirror: Mirror.ProductOf[T], tupleLens: TupleLens[ElemTypes, I]) extends Lens[T, Tuple.Elem[ElemTypes, I]] {
+  type ElemType = Tuple.Elem[ElemTypes, I]
+
+  def get (t: T): ElemType = {
     tupleLens.get(Tuple.fromProduct(t).asInstanceOf[ElemTypes])
   }
-  def set (t: T)(e: Tuple.Elem[ElemTypes, I]): T = {
+  def set (t: T)(e: ElemType): T = {
     val tuple = tupleLens.set(Tuple.fromProduct(t).asInstanceOf[ElemTypes])(e)  
     mirror.fromProduct(tuple.asInstanceOf[Product])
   }
