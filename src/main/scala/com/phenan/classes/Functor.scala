5c6854a266eeb94c3b399ab4c44eca35347b4eda
Kazuhiro Ichikawa <ichikawa@five-corp.com>
2020-02-08 23:05:09 +0900

    implement extension method for functor

--- src/main/scala/com/phenan/classes/Functor.scala
+++ src/main/scala/com/phenan/classes/Functor.scala
@@ -3,5 +3,7 @@ package com.phenan.classes
 trait Functor [F[_]] extends InvariantFunctor[F] {
   def map [A, B] (f: A => B): F[A] => F[B]
 
+  def [A, B] (fa: F[A]) map (f: A => B): F[B] = map(f)(fa)
+
   override def xmap [A, B] (f: A <=> B): F[A] <=> F[B] = Iso(map(f.from))(map(f.to))
 }
