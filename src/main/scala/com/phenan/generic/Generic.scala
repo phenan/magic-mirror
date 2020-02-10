04e7619979e1343907691e247a451415b392f27d
Kazuhiro Ichikawa <ichikawa@five-corp.com>
2020-02-10 23:30:23 +0900

    reimplement generic for correct type checking

--- src/main/scala/com/phenan/generic/Generic.scala
+++ src/main/scala/com/phenan/generic/Generic.scala
@@ -3,21 +3,20 @@ package com.phenan.generic
 import com.phenan.classes._
 import com.phenan.util._
 
-import scala.deriving.EmptyProduct
+import scala.deriving._
 
 trait Generic [T, R] extends <=>[R, T]
 
-given genericFromSingletonMirror [T] (using mirror: SingletonMirror[T]): Generic[T, Unit] {
-  def from: Unit => T = _ => mirror.fromProduct(EmptyProduct)
-  def to: T => Unit = _ => ()
+given genericFromProductMirror [T <: Product, R <: Tuple] (using mirror: Mirror.ProductOf[T], proof: mirror.MirroredElemTypes =:= R): Generic[T, R] {
+  def from: R => T = {
+    case p: Product => mirror.fromProduct(p)
+    case ()         => mirror.fromProduct(EmptyProduct)
+    case t          => mirror.fromProduct(new ArrayProduct(t.toArray))
+  }
+  def to: T => R = t => Tuple.fromProductTyped(t).asInstanceOf[R]
 }
 
-given genericFromProductMirror [T <: Product, R <: Product] (using mirror: ProductMirror[T, R]): Generic[T, R] {
-  def from: R => T = mirror.fromProduct
-  def to: T => R = Tuple.fromProductTyped
-}
-
-given genericFromSumMirror [T, R <: NonEmptyTuple] (using mirror: SumMirror[T, R]): Generic[T, Union[R]] {
+given genericFromSumMirror [T, R <: NonEmptyTuple] (using mirror: Mirror.SumOf[T], proof: mirror.MirroredElemTypes =:= R): Generic[T, Union[R]] {
   def from: Union[R] => T = _.asInstanceOf[T]
   def to: T => Union[R] = _.asInstanceOf[Union[R]]
 }
--- src/main/scala/com/phenan/util/Mirrors.scala
+++
@@ -1,9 +0,0 @@
-package com.phenan.util
-
-import scala.deriving.Mirror
-
-type ProductMirror [T, R <: Product] = Mirror.ProductOf[T] { type MirroredElemTypes = R }
-
-type SumMirror [T, R] = Mirror.SumOf[T] { type MirroredElemTypes = R }
-
-type SingletonMirror [T] = Mirror.ProductOf[T] { type MirroredElemTypes = Unit }
