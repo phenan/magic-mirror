5bb8de2c5f8a2e0e39f3c6f3703b9e57e946edac
Kazuhiro Ichikawa <ichikawa@five-corp.com>
2020-02-22 15:51:37 +0900

    implement buid method in HKD

--- src/main/scala/com/phenan/hkd/HKD.scala
+++ src/main/scala/com/phenan/hkd/HKD.scala
@@ -1,5 +1,7 @@
 package com.phenan.hkd
 
+import com.phenan.classes._
+import com.phenan.generic._
 import com.phenan.lens._
 import com.phenan.util._
 
@@ -16,10 +18,15 @@ object HKD {
 
 trait HKStruct [R <: Product, F[_]] extends Dynamic {
   def selectDynamic [L <: Singleton] (tag: L)(using mirror: Mirror.ProductOf[R], contains: Tuples.Contain[mirror.MirroredElemLabels, L], tupleLens: HKTupleLens[mirror.MirroredElemTypes, F, Tuples.IndexOf[mirror.MirroredElemLabels, L]]): F[Tuple.Elem[mirror.MirroredElemTypes, Tuples.IndexOf[mirror.MirroredElemLabels, L]]]
+  def build (using mirror: Mirror.ProductOf[R], foldable: HKTupleFoldable[mirror.MirroredElemTypes], monoidalInv: MonoidalInvariantFunctor[F]): F[R]
 }
 
 class HKTupledStruct [R <: Product, T <: Tuple, F[_]] (underlying: HKTuple[T, F]) extends HKStruct[R, F] {
   def selectDynamic [L <: Singleton] (tag: L)(using mirror: Mirror.ProductOf[R], contains: Tuples.Contain[mirror.MirroredElemLabels, L], tupleLens: HKTupleLens[mirror.MirroredElemTypes, F, Tuples.IndexOf[mirror.MirroredElemLabels, L]]): F[Tuple.Elem[mirror.MirroredElemTypes, Tuples.IndexOf[mirror.MirroredElemLabels, L]]] = {
     tupleLens.get(underlying.asInstanceOf[HKTuple[mirror.MirroredElemTypes, F]]).asInstanceOf[F[Tuple.Elem[mirror.MirroredElemTypes, Tuples.IndexOf[mirror.MirroredElemLabels, L]]]]
+  }
+  def build (using mirror: Mirror.ProductOf[R], foldable: HKTupleFoldable[mirror.MirroredElemTypes], monoidalInv: MonoidalInvariantFunctor[F]): F[R] = {
+    val generic = ProductGeneric[R, mirror.MirroredElemTypes](mirror)
+    HKTuple.construct[mirror.MirroredElemTypes, R, F](underlying.asInstanceOf[HKTuple[mirror.MirroredElemTypes, F]])(using foldable, monoidalInv, generic)
   }
 }
--- src/test/scala/com/phenan/hkd/HKDTest.scala
+++ src/test/scala/com/phenan/hkd/HKDTest.scala
@@ -1,5 +1,6 @@
 package com.phenan.hkd
 
+import com.phenan.classes._
 import com.phenan.lens.{given _}
 import com.phenan.util.{given _, _}
 
@@ -9,9 +10,33 @@ import org.junit.Assert._
 case class Foo (a: Int, b: String)
 
 class HKDTest {
+  given optionMonoidalInvariantFunctor : MonoidalInvariantFunctor[Option] {
+    def product [A, B <: Tuple] (a: => Option[A], b: => Option[B]): Option[A *: B] = for {
+      x <- a
+      y <- b
+    } yield x *: y
+
+    def pure [A] (a: => A): Option[A] = Some(a)
+
+    def xmap [A, B] (f: A <=> B): Option[A] <=> Option[B] = new Iso[Function1, Option[A], Option[B]] {
+      def from: Option[A] => Option[B] = _.map(f.from)
+      def to: Option[B] => Option[A] = _.map(f.to)
+    }
+  }
+
   @Test def testHKDGet (): Unit = {
     val hkd = HKD[Foo, Option](Some(10), Some("foo"))
     assertEquals(Some(10), hkd.a)
     assertEquals(Some("foo"), hkd.b)
+  }
+
+  @Test def testHKDBuildSome (): Unit = {
+    val hkd = HKD[Foo, Option](Some(20), Some("bar"))
+    assertEquals(Some(Foo(20, "bar")), hkd.build)
+  }
+
+  @Test def testHKDBuildNone (): Unit = {
+    val hkd = HKD[Foo, Option](None, Some("bar"))
+    assertEquals(None, hkd.build)
   }
 }
