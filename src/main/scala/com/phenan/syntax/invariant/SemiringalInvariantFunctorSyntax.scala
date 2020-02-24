5e3aeca092fdb740d93079934bbe83500a211153
Kazuhiro Ichikawa <ichikawa@five-corp.com>
2020-02-24 16:01:40 +0900

    do not explicitly specify type argument in SemiringalInvariantFunctorSyntax.rep

--- src/main/scala/com/phenan/syntax/invariant/SemiringalInvariantFunctorSyntax.scala
+++ src/main/scala/com/phenan/syntax/invariant/SemiringalInvariantFunctorSyntax.scala
@@ -7,12 +7,11 @@ import com.phenan.hkd.{given _, _}
 
 object SemiringalInvariantFunctorSyntax {
   def rep [F[_] : Semiringal : InvariantFunctor, T] (ft: F[T]): F[List[T]] = {
-    HKTuple.bundle[List[T]](
+    HKTuple.bundle(
       HKTuple[(::[T], scala.collection.immutable.Nil.type), F](
-        HKTuple.construct[::[T]](HKTuple[(T, List[T]), F](ft, rep(ft))),
-        HKTuple.construct[scala.collection.immutable.Nil.type](HKTuple[Unit, F](()))
+        HKTuple.construct(HKTuple[(T, List[T]), F](ft, rep(ft))),
+        HKTuple.construct(HKTuple[Unit, F](()))
       )
     )
   }
 }
-
